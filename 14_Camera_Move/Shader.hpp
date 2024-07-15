b738b3905e6df82cb57935251a8d8b5d5cffa558
PR5GRAMM2R <pon07151@gmail.com>
2024-07-15 13:31:24 +0900

    --all

---
+++ 14_Camera_Move/Shader.hpp
@@ -0,0 +1,26 @@
+#ifndef __SHADER__
+#define __SHADER__
+
+#include <glad/glad.h>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+class Shader
+{
+public :
+	unsigned int ID;	// 컴파일한 쉐이더들을 사용하기 위해 쉐이더들을 shaderProgram 으로 연결해주어야 함
+
+	Shader(const char* vertexPath, const char* fragmentPath);
+
+	void use();
+	void setBool(const std::string& name, bool value) const;
+	void setInt(const std::string& name, int value) const;
+	void setFloat(const std::string& name, float value) const;
+	void setMat4(const std::string& name, glm::mat4 value) const;
+};
+
+#endif
